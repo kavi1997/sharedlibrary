d38a876f743aee572a8e9e0b5802891107f3aba3
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-28 22:16:30 +0530

    Create nexusconnector.groovy

---
+++ vars/nexusconnector.groovy
@@ -0,0 +1,30 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createProject(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def id = '"'+resultJson.id+'"'
+def name= '"'+name+'"'
+ 
+ httpRequest authentication: 'jira_password', 
+    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']], 
+    httpMode: 'POST', requestBody: """{
+       "repoType": "hosted",
+        "id": ${id},
+        "name": ${name},
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"
+    
+}""", responseHandle: 'NONE', url: 'http://3.15.18.214:8081/nexus/service/local/repositories'
+
+ 
+}
+def call(){
+ def request = libraryResource 'data.json'
+ createProject(request)
+}
