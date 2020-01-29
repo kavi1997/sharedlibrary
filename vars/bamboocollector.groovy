39cd391b9dd819f67dfda0217ef52f9edef3050b
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 23:19:19 +0530

    Update bamboocollector.groovy

--- vars/bamboocollector.groovy
+++ vars/bamboocollector.groovy
@@ -6,7 +6,7 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parseText(data)
 def key= resultJson.key
 
-httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+httpRequest authentication: 'bamboo', contentType: "APPLICATION_JSON", 
     
     httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${key}.json"
 }
