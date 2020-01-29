85306212a48cb3b0c0137f64f4626a63cd4c7c93
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 18:34:31 +0530

    Update nexuscollector.groovy

--- vars/nexuscollector.groovy
+++ vars/nexuscollector.groovy
@@ -1,8 +1,4 @@
 
 def call(){
-curl -X GET \
-  http://3.15.18.214:8081/nexus/service/local/repositories/id6 \
-  -H 'authorization: Basic YWRtaW46YWRtaW4xMjM=' \
-  -H 'cache-control: no-cache' \
-  -H 'postman-token: 85f6fa06-d282-6035-0a28-d7ceb8cbf2c2'
+curl -X GET -v -u admin:admin123 -H "ContentType: application/json; charset=UTF-8" "http://3.15.18.214:8081/nexus/service/local/repositories/id6"
   }
