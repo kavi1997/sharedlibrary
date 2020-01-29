998223981e7cae5c351d75d29e5a77c0082e960a
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 14:54:03 +0530

    Update nexusconector.groovy

--- vars/nexusconector.groovy
+++ vars/nexusconector.groovy
@@ -7,12 +7,19 @@ def resultJson = jsonSlurper.parseText(data)
 def repoName = resultJson.name
 //def rid = resultJson.id
 //def projUrl = resultJson.url
-httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', 
+    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
-    "name": "${repoName}",
-    "scmId": "git",
-    "forkable": true
-}""", responseHandle: 'NONE', url: "http://3.15.18.214:8081/nexus/service/local/repositories"
+   "repoType": "hosted",
+        "id": "somerepo2",
+        "name": ${repoName},
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"
+   
+}""", responseHandle: 'NONE', url: 'http://3.15.18.214:8081/nexus/service/local/repositories'
 }
 def call(){
 def request = libraryResource 'data.json'
