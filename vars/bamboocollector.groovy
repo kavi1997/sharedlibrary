43e014fd0140613e3d075d7459f7e157b2855129
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 23:24:07 +0530

    Update bamboocollector.groovy

--- vars/bamboocollector.groovy
+++ vars/bamboocollector.groovy
@@ -8,7 +8,7 @@ def key= resultJson.key
 
 httpRequest authentication: 'bamboo', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${key}.json"
+    httpMode: 'GET', url: "http://18.220.143.53:8085/rest/api/latest/plan/${key}.json"
 }
 	def call(){
 def request = libraryResource 'bamboolist.json'
