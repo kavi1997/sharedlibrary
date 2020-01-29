8d2f7cb8f68558b136579d197c78c2683a9ee7a7
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 23:15:59 +0530

    Create bamboocollector.groovy

---
+++ vars/bamboocollector.groovy
@@ -0,0 +1,16 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+detailplan(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def key= resultJson.key
+
+httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${key}.json"
+}
+	def call(){
+def request = libraryResource 'bamboolist.json'
+detailplan(request)
+}
