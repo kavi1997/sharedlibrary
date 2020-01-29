724ca60ab367a7f706e35b1e6807b6ae4cd49924
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 18:52:50 +0530

    Create nexuscolector.groovy

---
+++ vars/nexuscolector.groovy
@@ -0,0 +1,18 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+showRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+//def repoName = resultJson.name
+def rid = resultJson.id
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${rid}"
+}
+	def call(){
+def request = libraryResource 'detail.json'
+showRepo(request)
+}
