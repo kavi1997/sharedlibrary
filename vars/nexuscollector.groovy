eb815c6f0d891d8748520be829e7f787ed98f236
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 18:38:17 +0530

    Update nexuscollector.groovy

--- vars/nexuscollector.groovy
+++ vars/nexuscollector.groovy
@@ -1,5 +1,5 @@
 
 def call(){
-curl -X GET -v -u admin:admin123 -H "ContentType: application/json; charset=UTF-8" "http://3.15.18.214:8081/nexus/service/local/repositories/id6"
+sh 'curl -X GET -v -u admin:admin123 -H "ContentType: application/json; charset=UTF-8" "http://3.15.18.214:8081/nexus/service/local/repositories/id6"'
   }
 
