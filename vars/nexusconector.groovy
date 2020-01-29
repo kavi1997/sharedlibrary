4a60233eb5829e4661324e9df7069cfff2f82301
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 16:47:57 +0530

    Update nexusconector.groovy

--- vars/nexusconector.groovy
+++ vars/nexusconector.groovy
@@ -7,7 +7,7 @@ def resultJson = jsonSlurper.parseText(data)
 def repoName = resultJson.name
 def rid = resultJson.id
 //def projUrl = resultJson.url
-def  call(){
+
 httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
     
     httpMode: 'POST', requestBody: 
