5149ea629ad3648dcee61833bc2102bf8e899123
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 16:05:12 +0530

    Update nexusconector.groovy

--- vars/nexusconector.groovy
+++ vars/nexusconector.groovy
@@ -1,14 +1,13 @@
-//import groovy.json.JsonSlurper 
+import groovy.json.JsonSlurper 
 
-//@NonCPS
-/*createRepo(String data){
+@NonCPS
+createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def repoName = resultJson.name
-def rid = resultJson.id*/
+def rid = resultJson.id
 //def projUrl = resultJson.url
-def call()
-{
+
 httpRequest authentication: 'nexus_cred', contentType: 'APPLICATION_JSON', 
     
     httpMode: 'POST', requestBody: """{
@@ -16,8 +15,8 @@ httpRequest authentication: 'nexus_cred', contentType: 'APPLICATION_JSON',
     	"data":
 	{
 		"repoType": "hosted",
-        "id": "id4",
-        "name": "repo4",
+        "id": ${rid},
+        "name": ${repoName},
         "repoPolicy": "RELEASE",
         "provider": "maven2",
         "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
@@ -30,7 +29,7 @@ httpRequest authentication: 'nexus_cred', contentType: 'APPLICATION_JSON',
    
 }""", responseHandle: 'NONE', url: 'http://3.15.18.214:8081/nexus/service/local/repositories'
 }
-/*def call(){
+def call(){
 def request = libraryResource 'data.json'
 createRepo(request)
-}*/
+}
