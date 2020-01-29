618a4839bb0220202c84cf7da41c2f5c695c73af
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 22:36:46 +0530

    Update bambooconnector.groovy

--- vars/bambooconnector.groovy
+++ vars/bambooconnector.groovy
@@ -1,31 +1,29 @@
 import groovy.json.JsonSlurper 
 
-/*@NonCPS
+@NonCPS
 createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-def name = resultJson.stage*/
-//def projUrl = resultJson.url
-def call()
-{
+def name = resultJson.stage
+
 httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
     
     httpMode: 'POST', requestBody: 
   """{
 "stage":[
 {
-"stage"	:"TestStage",
+"stage"	:"${name}",
 "Job":"new"
 },
 {
-"stage"	:"TestStage",
+"stage"	:"${name}",
 "Job":"one"
 }
 ]
 
 }""" ,url: "http://18.220.143.53:8085/rest/api/latest/queue/LAT-WEB"
 }
-/*def call(){
-def request = libraryResource 'data.json'
+def call(){
+def request = libraryResource 'bamboo.json'
 createRepo(request)
-}*/
+}
