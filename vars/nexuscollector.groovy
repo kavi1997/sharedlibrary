74e373b5bc36a28009f4e81b94cd121b70b68a0e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 18:30:32 +0530

    Create nexuscollector.groovy

---
+++ vars/nexuscollector.groovy
@@ -0,0 +1,8 @@
+
+def call(){
+curl -X GET \
+  http://3.15.18.214:8081/nexus/service/local/repositories/id6 \
+  -H 'authorization: Basic YWRtaW46YWRtaW4xMjM=' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: 85f6fa06-d282-6035-0a28-d7ceb8cbf2c2'
+  }
