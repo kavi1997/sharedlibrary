a4aee9a0fbdf9f680ba0c63eac382bcf9c8fdb8b
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 18:47:34 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,6 +1,6 @@
 import jenkins.model.*
- jenkins = Jenkins.instance
-def call(String msg ="msg")
+ 
+def call(String msg ='msg')
 {
-  sh 'echo "${msg}">>log.txt'
+  sh 'echo "'${msg}'">>log.txt'
 }
