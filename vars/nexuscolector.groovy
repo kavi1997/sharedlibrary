8572450ae6e4ae03cc1dad12fd392992fcf980aa
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 11:56:01 +0530

    Update nexuscolector.groovy

--- vars/nexuscolector.groovy
+++ vars/nexuscolector.groovy
@@ -8,7 +8,7 @@ def resultJson = jsonSlurper.parseText(data)
 def rid = resultJson.id
 //def projUrl = resultJson.url
 
-httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", Accept: "APPLICATION/JSON",
+httpRequest authentication: 'nexus_cred', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']],contentType: "APPLICATION_JSON", 
     
     httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${rid}"
 }
