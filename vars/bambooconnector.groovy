a666d47c8f68a9f3dd4ee5f02cb8fcfcca12d7b1
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 22:46:30 +0530

    Update bambooconnector.groovy

--- vars/bambooconnector.groovy
+++ vars/bambooconnector.groovy
@@ -23,15 +23,15 @@ httpRequest authentication: 'bamboo', contentType: "APPLICATION_JSON",
 
 }""" ,url: "http://18.220.143.53:8085/rest/api/latest/queue/${key}"
 }
-exeStage(String data){
+/*exeStage(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def response=httpRequest 'http://18.220.143.53:8085/rest/api/latest/queue/LAT-WEB'
 def Status=response.status
 println(response.status)
-}
+}*/
 def call(){
 def request = libraryResource 'bamboo.json'
 exeStage(request)
-log(request)
+/*log(request)
 }
