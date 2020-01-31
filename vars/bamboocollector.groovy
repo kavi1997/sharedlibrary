279cef4eae6c0c573b9a440f4621d3d12212aeee
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 12:19:39 +0530

    Update bamboocollector.groovy

--- vars/bamboocollector.groovy
+++ vars/bamboocollector.groovy
@@ -4,11 +4,11 @@ import groovy.json.JsonSlurper
 detailplan(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-def key= resultJson.key
+def key= resultJson.bambookey
 
 httpRequest authentication: 'bamboo', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'GET', url: "http://18.220.143.53:8085/rest/api/latest/plan/${key}.json"
+    httpMode: 'GET', url: "http://18.220.143.53:8085/rest/api/latest/plan/${bambookey}.json"
 }
 	def call(){
 def request = libraryResource 'bamboolist.json'
