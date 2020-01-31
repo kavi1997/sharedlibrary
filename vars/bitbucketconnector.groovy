c9e3ece42a9dfabd64830778d4303bdd32078359
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 12:54:19 +0530

    Create bitbucketconnector.groovy

---
+++ vars/bitbucketconnector.groovy
@@ -0,0 +1,27 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def repoName = resultJson.bitbucketname
+def projUrl = resultJson.bitbucketurl
+//def projUrl = resultJson.url
+httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${repoName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url: "${projUrl}"
+  
+  }
+
+
+def call(){
+def request = libraryResource 'data.json'
+createRepo(request)
+
+
+
+
+}
