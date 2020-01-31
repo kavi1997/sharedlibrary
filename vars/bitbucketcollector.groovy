2ec3dabfb248bcf49d2ae63acbc2b14d789998eb
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 12:55:21 +0530

    Create bitbucketcollector.groovy

---
+++ vars/bitbucketcollector.groovy
@@ -0,0 +1,16 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projUrl = resultJson.bitbucketurl
+
+httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'GET', requestBody: """
+{
+}""", responseHandle: 'NONE', url: "${projUrl}"
+}
+def call(){
+def request = libraryResource 'data.json'
+createRepo(request)
+}
