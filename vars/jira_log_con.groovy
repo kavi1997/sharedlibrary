fe61d1b3ee8bc0a192e837f8550587e5d4e86698
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 14:21:03 +0530

    Create jira_log_con.groovy

---
+++ vars/jira_log_con.groovy
@@ -0,0 +1,33 @@
+import groovy.json.JsonSlurper 
+@NonCPS
+/*createlog(String data, string message){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projectName = resultJson.key
+sh "echo ${message} >log.txt"
+/*def fileName = "jira.txt"
+def inputFile = new File(fileName)
+if(inputFile.exists())
+  {
+    inputFile.append("${projectName}")
+  }
+  else
+  {
+     inputFile.write("${projectName}")
+  }
+}*/
+//}
+def call(message)
+{
+ println(message)
+ def request = libraryResource 'jira.json'
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ def projectName = resultJson.name
+  Date date = new Date() 
+  sh " echo '${date}' ${message} with the name '${projectName}' >>log.txt"
+}
+/*def call(){
+def request = libraryResource 'data1.json'
+createlog(request)
+}*/
