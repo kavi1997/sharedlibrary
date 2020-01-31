b89bd940453e57d04766c268798b2283b93f1e15
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 11:49:18 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,4 +1,11 @@
+import groovy.json.JsonSlurper 
+@NonCPS
 def call(message)
 {
- sh "echo ${message} >>log.txt"
+ def request = libraryResource 'bamboo1.json'
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ def key = resultJson.key
+  Date date = new Date() 
+ sh " echo '${date}' ${message} '${key}'>>log.txt"
 }
