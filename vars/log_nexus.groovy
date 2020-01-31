63fb2ca3939e871005873c6cc37fd367c7fe58ab
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 12:57:51 +0530

    Update log_nexus.groovy

--- vars/log_nexus.groovy
+++ vars/log_nexus.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurper
 def call(message)
 {
  println(message)
- def request = libraryResource 'data.json'
+ def request = libraryResource 'nexus.json'
  def jsonSlurper = new JsonSlurper() 
  def resultJson = jsonSlurper.parseText(request)
  def rid = resultJson.id
