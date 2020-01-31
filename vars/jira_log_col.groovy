378e4be12234c204aa27f226ac0c68fd4d4ab88d
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 14:21:39 +0530

    Create jira_log_col.groovy

---
+++ vars/jira_log_col.groovy
@@ -0,0 +1,13 @@
+import groovy.json.JsonSlurper 
+@NonCPS
+def call(message)
+{
+ println(message)
+ def request = libraryResource 'jira.json'
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ def projectName = resultJson.key
+  Date date = new Date() 
+  sh " echo '${date}' ${message} with the name '${projectName}' >>log.txt"
+}
+© 2020 GitHu
