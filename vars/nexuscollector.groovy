5760d9d23fdd28e322ff8ccb1247180754b8c7cb
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 15:44:13 +0530

    Delete nexuscollector.groovy

--- vars/nexuscollector.groovy
+++
@@ -1,5 +0,0 @@
-
-def call(){
-sh 'curl -X GET -v -u admin:admin123 -H "ContentType: application/json; charset=UTF-8" -d "@detail.json" "http://3.15.18.214:8081/nexus/service/local/repositories/${id}"'
-  }
-
