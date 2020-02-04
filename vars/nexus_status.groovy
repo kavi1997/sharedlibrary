4b893d1cca957e77d1d41829540287eff0117ad4
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-04 14:48:32 +0530

    Create nexus_status.groovy

---
+++ vars/nexus_status.groovy
@@ -0,0 +1,18 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+showRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+//def repoName = resultJson.name
+def rid = resultJson.id
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'nexus_cred',contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${rid}/status"
+}
+	def call(){
+def request = libraryResource 'nexus.json'
+showRepo(request)
+}
