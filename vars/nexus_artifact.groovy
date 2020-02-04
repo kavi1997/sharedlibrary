4168ed313d39dd0b5d36393dbf93d4850fff4a37
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-04 17:52:24 +0530

    Update nexus_artifact.groovy

--- vars/nexus_artifact.groovy
+++ vars/nexus_artifact.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh 'curl -X GET -i -H  "Accept: application/json" -H "Content-Type: application/json" -d "@repo.json" -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/artifact/maven/redirect?r=repo&g=Codekillers&a=dynamic_new&v=RELEASE"'
+sh 'curl -X GET -i -H  "Accept: application/json" -H "Content-Type: application/json" /*-d "@repo.json" */-u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/artifact/maven/redirect?r=repo&g=Codekillers&a=dynamic_new&v=RELEASE"'
 }
