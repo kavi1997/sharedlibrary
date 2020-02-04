66ee5cf943b661528babb3162f7ec5ce91e1f948
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-04 14:54:28 +0530

    Update nexus_status.groovy

--- vars/nexus_status.groovy
+++ vars/nexus_status.groovy
@@ -8,7 +8,7 @@ def resultJson = jsonSlurper.parseText(data)
 def rid = resultJson.id
 //def projUrl = resultJson.url
 
-httpRequest authentication: 'nexus_cred',contentType: "APPLICATION/JSON", 
+httpRequest authentication: 'nexus_cred',contentType: "application/json", 
     
     httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${rid}/status"
 }
