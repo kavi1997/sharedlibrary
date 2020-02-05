a7d6d0972c7ed31e579c8e35e2016e892a03791d
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 01:05:31 +0530

    Create Nchange_pwd.groovy

---
+++ vars/Nchange_pwd.groovy
@@ -0,0 +1,30 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.brm)
+
+String a=jsonObj.brm.password.oldpassword
+String oldpass=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.password.userId
+String userid=b.replaceAll("\\[", "").replaceAll("\\]","");
+String c=jsonObj.brm.password.newPassword
+String newpass=c.replaceAll("\\[", "").replaceAll("\\]","");
+	println(repoName)
+	println(repoid)
+	
+
+httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST', requestBody: 
+  """{
+    	"data":
+	{
+        "oldPassword":${oldpass},
+"userId":${userid},
+"newPassword":${newpass}
+             
+	}
+        
+   }""", url: "http://3.15.18.214:8081/nexus/service/local/users_setpw"
+}
