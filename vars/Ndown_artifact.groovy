254d3df6190fd744e1aa40e9c0182cf2bbe16fbe
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-05 19:33:14 +0530

    Update Ndown_artifact.groovy

--- vars/Ndown_artifact.groovy
+++ vars/Ndown_artifact.groovy
@@ -8,9 +8,10 @@ def jsonObj = readJSON text: jsonString
 //String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.data.id
 String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war' "
 
-httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+/*httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war"
+    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war" */
     
 }
