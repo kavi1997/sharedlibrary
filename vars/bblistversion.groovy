c4cda1ce44990c177062e628c665655a02c91aa6
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 23:51:41 +0530

    Create bblistversion.groovy

---
+++ vars/bblistversion.groovy
@@ -0,0 +1,9 @@
+def call(JSON){
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+
+String a=jsonObj.ci.id.deploymentProjectId
+String id=a.replaceAll("\\[", "").replaceAll("\\]","");
+println(id)
+sh 'curl -X GET -i -H  "Accept: application/json" -H "Content-Type: application/json" -d "@JSON" -u rig:rigaDapt@devOps " http://18.220.143.53:8085/rest/api/latest/deploy/project/${id}/versions"'
+}
