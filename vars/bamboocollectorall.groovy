55aed5e2514c2330fcf5a4b4ab143dc9fbc0885c
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 01:27:44 +0530

    Update and rename bblistparticulardeployproj.groovy to bamboocollectorall.groovy

---
+++ vars/bamboocollectorall.groovy
@@ -0,0 +1,12 @@
+def call(JSON){
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+
+String a=jsonObj.ci.id.deploymentProjectId
+String id=a.replaceAll("\\[", "").replaceAll("\\]","");
+println(id)
+sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/plan.json" '
+sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/project.json" '
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
+ sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}'/versions "  
+}
