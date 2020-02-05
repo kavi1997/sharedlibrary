c52720929563c1b577834ed8d62c1e60a2e7e876
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 13:03:22 +0530

    Update nexus_artifact_fetch.groovy

--- vars/nexus_artifact_fetch.groovy
+++ vars/nexus_artifact_fetch.groovy
@@ -1,4 +1,5 @@
 def call()
 {
-sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war' "
+sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/artifact/maven/content?g=Codekillers&a=dynamic_new&v=1.0&r=repo' "
+ "
 }
