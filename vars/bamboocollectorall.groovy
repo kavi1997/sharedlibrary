e6416aea8e707007f000ce91f99857a480baaf9c
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 01:41:25 +0530

    Update bamboocollectorall.groovy

--- vars/bamboocollectorall.groovy
+++ vars/bamboocollectorall.groovy
@@ -4,7 +4,7 @@ def jsonObj = readJSON text: jsonString
 
 String a=jsonObj.ci.id.deploymentProjectId
 String id=a.replaceAll("\\[", "").replaceAll("\\]","");
- String b=jsonObj.ci.projectkey.key
+ String b=jsonObj.ci.list.key
 String key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(id)
 sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/plan.json" '
