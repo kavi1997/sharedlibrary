161a0a5d167928522b60ee211c2616b77bb2e0f7
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 01:42:55 +0530

    Delete bblistallpalnproj.groovy

--- vars/bblistallpalnproj.groovy
+++
@@ -1,5 +0,0 @@
-def call()
-{
-sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/plan.json" '
-sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/project.json" '
-}
