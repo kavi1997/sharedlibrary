f7fdfdf993900e885a680036ef537a1a48e88548
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 22:30:13 +0530

    Create bbaddlabelbuild.groovy

---
+++ vars/bbaddlabelbuild.groovy
@@ -0,0 +1,23 @@
+def call(JSON){
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+println(jsonObj.ci)
+
+String a=jsonObj.ci.label.name
+String name=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.ci.project-plankey.key
+String key=b.replaceAll("\\[", "").replaceAll("\\]","");
+String c=jsonObj.ci.Build.buildNumber
+String bno=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+println(name)
+println(key)
+println(bno)
+
+
+httpRequest authentication: 'bamboo', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+    {
+	"name":"${name}"
+}
+}""", responseHandle: 'NONE', url:"http://18.220.143.53:8085/rest/api/latest/result/${key}-${bno}/label"
+ }
