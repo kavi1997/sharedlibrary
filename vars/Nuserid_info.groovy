f00234cb0a97b87accccca6241632a4050256b5b
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 01:42:04 +0530

    Update Nuserid_info.groovy

--- vars/Nuserid_info.groovy
+++ vars/Nuserid_info.groovy
@@ -8,9 +8,10 @@ def jsonObj = readJSON text: jsonString
 //String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.brm.user.userId
 String userid=b.replaceAll("\\[", "").replaceAll("\\]","");
+sh "curl -X GET -i -H  -d  -u admin:admin123 http://3.15.18.214:8081/nexus/service/local/users/${userid} "
 
-httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+//httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/users/${userid}"
+  //  httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/users/${userid}"
     
 }
