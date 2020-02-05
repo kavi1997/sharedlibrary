bcc60e32ff54bb63d9f88aa95f1a180b4a8f4c46
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 22:42:38 +0530

    Create bbaddcomment.groovy

---
+++ vars/bbaddcomment.groovy
@@ -0,0 +1,24 @@
+def call(JSON){
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+println(jsonObj.ci)
+
+String a=jsonObj.ci.label.content
+String comment=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.ci.project-plankey.key
+String key=b.replaceAll("\\[", "").replaceAll("\\]","");
+String c=jsonObj.ci.Build.buildNumber
+String bno=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+println(comment)
+println(key)
+println(bno)
+
+
+httpRequest authentication: 'bamboo', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+   {
+
+    "content":"${comment}"
+}
+}""", responseHandle: 'NONE', url:"http://18.220.143.53:8085/rest/api/latest/result/${key}-${bno}/comment"
+ }
