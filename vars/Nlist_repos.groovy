2e9fe5705e9b6156562b7b1271f7b2bae1855ade
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 01:32:22 +0530

    Update Nlist_repos.groovy

--- vars/Nlist_repos.groovy
+++ vars/Nlist_repos.groovy
@@ -8,9 +8,10 @@ def jsonObj = readJSON text: jsonString
 //String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.brm.repositories.repository.id
 String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+sh 'curl -X GET -i -H  -d  -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories" '
 
-httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+//httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories"
+   // httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories"
     
 }
