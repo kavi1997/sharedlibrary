0f04ddf4699e908dce81a922d1c52a99ed8048ac
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 12:42:33 +0530

    Update nexus_artifact.groovy

--- vars/nexus_artifact.groovy
+++ vars/nexus_artifact.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-wget --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war"
+sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war' "
 }
