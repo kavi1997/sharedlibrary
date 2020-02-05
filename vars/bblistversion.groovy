c0f6966144398b1ecc7be7b89e1aedbf06c8473d
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 00:18:34 +0530

    Update bblistversion.groovy

--- vars/bblistversion.groovy
+++ vars/bblistversion.groovy
@@ -5,5 +5,5 @@ def jsonObj = readJSON text: jsonString
 String a=jsonObj.ci.id.deploymentProjectId
 String id=a.replaceAll("\\[", "").replaceAll("\\]","");
 println(id)
-sh 'curl -X GET -i -H  "Accept: application/json" -H "Content-Type: application/json" -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/deploy/project/$id/versions"'
+sh "curl -X GET -i -H  "Accept: application/json" -H "Content-Type: application/json" -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/deploy/project/${id}/versions" "
 }
