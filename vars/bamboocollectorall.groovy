fa15388d4d9c3e468f80c9bf732d9fcadeb3be66
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 01:38:41 +0530

    Update bamboocollectorall.groovy

--- vars/bamboocollectorall.groovy
+++ vars/bamboocollectorall.groovy
@@ -4,9 +4,12 @@ def jsonObj = readJSON text: jsonString
 
 String a=jsonObj.ci.id.deploymentProjectId
 String id=a.replaceAll("\\[", "").replaceAll("\\]","");
+ String b=jsonObj.ci.projectkey.key
+String key=a.replaceAll("\\[", "").replaceAll("\\]","");
 println(id)
 sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/plan.json" '
 sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/project.json" '
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
- sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}'/versions "  
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}'/versions " 
+ sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/project/'${key}'.json " 
 }
