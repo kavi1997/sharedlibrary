25d4efd723f0f19f70213481954f894f04f425ca
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 01:22:53 +0530

    Create bblistparticulardeployproj.groovy

---
+++ vars/bblistparticulardeployproj.groovy
@@ -0,0 +1,8 @@
+def call(JSON){
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+
+String a=jsonObj.ci.id.deploymentProjectId
+String id=a.replaceAll("\\[", "").replaceAll("\\]","");
+println(id)
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  }
