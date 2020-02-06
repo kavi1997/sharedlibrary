0d581093450066bdeb4fd21dde50a77123a05b6b
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 14:46:23 +0530

    Update Ndown_artifact.groovy

--- vars/Ndown_artifact.groovy
+++ vars/Ndown_artifact.groovy
@@ -10,7 +10,7 @@ String b=jsonObj.brm.password.fileName
 String fn=b.replaceAll("\\[", "").replaceAll("\\]","");
 
 
-httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+//httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
    
   sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/${fn}' "
     // httpMode: 'GET', wget --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war"
