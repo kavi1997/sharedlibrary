ed357e7fac6abf09e2d5d794cef0aad99836f984
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-07 00:33:38 +0530

    Update bbblog.groovy

--- vars/bbblog.groovy
+++ vars/bbblog.groovy
@@ -1,31 +1,12 @@
-def call(message,JSON)
+def call(message)
 {
   
-def jsonString = JSON
-def jsonObj = readJSON text: jsonString
-println(jsonObj.ci)
 
-String a=jsonObj.ci.projectplankey.key
-String b=jsonObj.ci.id.deploymentProjectId
-String c=jsonObj.ci.projectkey.key
-String plankey=a.replaceAll("\\[", "").replaceAll("\\]","");
-String deploymentprojectid=b.replaceAll("\\[", "").replaceAll("\\]","");
-String deletedprojectname=c.replaceAll("\\[", "").replaceAll("\\]","");
-  
  println(message)
   def mes=message
   Date date = new Date() 
-  if(message.contains(deployment))
-     {
-      sh " echo '${date}' Bamboo  ${message} '${deploymentprojectid}' >>log.txt"
-     }
-  else if(message.contains(deleted))
-          {
-              sh " echo '${date}' Bamboo  ${message} '${deletedname}' >>log.txt"
-          }
-     else
-     {
-       sh " echo '${date}' Bamboo  ${message} '${plankey}' >>log.txt"
-     }
+  
+      sh " echo '${date}' Bamboo  ${message}  >>log.txt"
+   
 }
-© 2020 GitHub, Inc.
+
