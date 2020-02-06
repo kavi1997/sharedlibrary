c3cac9735b336afc9e500040d0783caec6bc6460
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-06 23:27:18 +0530

    Update bblog.groovy

--- vars/bblog.groovy
+++ vars/bblog.groovy
@@ -7,17 +7,23 @@ println(jsonObj.ci)
 
 String a=jsonObj.ci.projectplankey.key
 String b=jsonObj.ci.id.deploymentProjectId
+String c=jsonObj.ci.projectkey.key
 String plankey=a.replaceAll("\\[", "").replaceAll("\\]","");
 String deploymentprojectid=b.replaceAll("\\[", "").replaceAll("\\]","");
+String deletedprojectname=c.replaceAll("\\[", "").replaceAll("\\]","");
   
  println(message)
   def mes=message
   Date date = new Date() 
-  if(message.contains(deploy))
+  if(message.contains(deployment))
      {
-      sh " echo '${date}' Bamboo  ${message} '${plankey}' >>log.txt"
+      sh " echo '${date}' Bamboo  ${message} '${deploymentprojectid}' >>log.txt"
      }
+  else if(message.contains(deleted)
+          {
+              sh " echo '${date}' Bamboo  ${message} '${deletedname}' >>log.txt"
+          }
      else
      {
-       sh " echo '${date}' Bamboo  ${message} '${deploymentprojectid}' >>log.txt"
+       sh " echo '${date}' Bamboo  ${message} '${plankey}' >>log.txt"
      }
