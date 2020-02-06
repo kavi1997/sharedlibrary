e4f4fde035fcb245f6383331e128b46a3fb7d918
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-06 19:14:41 +0530

    Update bamboocollectorall.groovy

--- vars/bamboocollectorall.groovy
+++ vars/bamboocollectorall.groovy
@@ -7,10 +7,12 @@ String id=a.replaceAll("\\[", "").replaceAll("\\]","");
  String b=jsonObj.ci.list.key
 String key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(id)
-sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/plan.json" '
-sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/project.json" '
+  withCredentials([string(credentialsId: 'bamboo', variable: 'bamboo_pwd')]){
+   sh 'curl -X GET -i -H  -d  -u ${bamboo_pwd} "http://18.220.143.53:8085/rest/api/latest/plan.json" '
+/*sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/project.json" '
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}'/versions " 
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/project/'${key}'.json " 
-sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/result/'${key}'.json?includeAllStates=true " 
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/result/'${key}'.json?includeAllStates=true "*/
+  }
 }
