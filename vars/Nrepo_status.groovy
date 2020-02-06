6273a47e57c70e1c9177ada5f326b00d8aa8d740
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 15:40:26 +0530

    Update Nrepo_status.groovy

--- vars/Nrepo_status.groovy
+++ vars/Nrepo_status.groovy
@@ -8,7 +8,7 @@ def jsonObj = readJSON text: jsonString
 //String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.brm.repositories.repository.id
 String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
-sh "curl -X GET -i -H  -d  -u admin:admin123 http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}/status | json_reformat"
+sh "curl -X GET -i -H  -d  -u admin:admin123 http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}/status"
 //httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
     
     //httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}/status"
