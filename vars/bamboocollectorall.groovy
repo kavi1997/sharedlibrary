c8f026b4b07e27342be56d42af484e4fc7cd832f
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-06 19:53:44 +0530

    Update bamboocollectorall.groovy

--- vars/bamboocollectorall.groovy
+++ vars/bamboocollectorall.groovy
@@ -7,7 +7,7 @@ String id=a.replaceAll("\\[", "").replaceAll("\\]","");
  String b=jsonObj.ci.list.key
 String key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(id)
- withCredentials([usernamePassword(credentialsId: "${bamboo}", passwordVariable: 'password', usernameVariable:'username')]) {
+ withCredentials([usernamePassword(credentialsId: 'bamboo', passwordVariable: 'password', usernameVariable:'username')]) {
 sh 'curl -X GET -i -H  -d  -u $username:$password "http://18.220.143.53:8085/rest/api/latest/plan.json" '
   sh 'curl -X GET -i -H  -d  -u $username:$password "http://18.220.143.53:8085/rest/api/latest/project.json" '
   sh "curl -X GET -i -H  -d  -u ${username}:${password} http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
