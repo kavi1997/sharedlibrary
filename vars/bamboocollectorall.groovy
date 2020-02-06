0ac8d2f87919cdcff331b663677cdf85dbb53e65
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-06 19:22:21 +0530

    Update bamboocollectorall.groovy

--- vars/bamboocollectorall.groovy
+++ vars/bamboocollectorall.groovy
@@ -7,8 +7,8 @@ String id=a.replaceAll("\\[", "").replaceAll("\\]","");
  String b=jsonObj.ci.list.key
 String key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(id)
-  withCredentials([string(credentialsId: 'bamboo', variable: 'bamboo_pwd')]){
-   sh 'curl -X GET -i -H  -d  -u ${bamboo_pwd} "http://18.220.143.53:8085/rest/api/latest/plan.json" '
+  withCredentials([usernamePassword(credentialsId: 'bamboo', passwordVariable: 'password', usernameVariable:'username')]) {
+   sh 'curl -X GET -i -H  -d  -u ${username}:${password} "http://18.220.143.53:8085/rest/api/latest/plan.json" '
 /*sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/project.json" '
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}'/versions " 
