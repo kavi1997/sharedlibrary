0acc1ea7800461d32541b1f51e658dd4eaf5ad99
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 14:49:23 +0530

    Create Ndelete_user.groovy

---
+++ vars/Ndelete_user.groovy
@@ -0,0 +1,16 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.brm)
+
+//String a=jsonObj.data.name
+//String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.repositories.repository.id
+String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'DELETE', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}"
+    
+}
