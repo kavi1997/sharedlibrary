5fb62a1f59ebd1ba4b6ade7daf46db72fa2c5722
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-06 19:37:22 +0530

    Update bamboocollectorall.groovy

--- vars/bamboocollectorall.groovy
+++ vars/bamboocollectorall.groovy
@@ -10,7 +10,7 @@ println(id)
   withCredentials([usernamePassword(credentialsId: 'bamboo', passwordVariable: 'password', usernameVariable:'username')]) {
    sh 'curl -X GET -i -H  -d  -u ${username}:${password} "http://18.220.143.53:8085/rest/api/latest/plan.json" '
 sh 'curl -X GET -i -H  -d  -u ${username}:${password} "http://18.220.143.53:8085/rest/api/latest/project.json" '
-sh "curl -X GET -i -H  -d  -u '${username}':'${password}' http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
+sh "curl -X GET -i -H  -d  -u "${username}":"${password}" http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}' "  
 sh "curl -X GET -i -H  -d  -u '${username}':'${password}' http://18.220.143.53:8085/rest/api/latest/deploy/project/'${id}'/versions " 
 sh "curl -X GET -i -H  -d  -u '${username}':'${password}' http://18.220.143.53:8085/rest/api/latest/project/'${key}'.json " 
 sh "curl -X GET -i -H  -d  -u ${username}:${password} http://18.220.143.53:8085/rest/api/latest/result/'${key}'.json?includeAllStates=true "
