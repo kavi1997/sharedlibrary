cbeb9f0c0d852901805b096ec3e8a9812356aae7
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 13:18:32 +0530

    Update Ndown_artifact.groovy

--- vars/Ndown_artifact.groovy
+++ vars/Ndown_artifact.groovy
@@ -4,15 +4,15 @@ def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 //println(jsonObj.brm)
 
-//String a=jsonObj.data.name
+//String a=jsonObj.brm.password.fileName
 //String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
-String b=jsonObj.data.id
-String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.password.fileName
+String fn=b.replaceAll("\\[", "").replaceAll("\\]","");
 
 
 //httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
    
-  sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war' "
+  sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/${fn}' "
     // httpMode: 'GET', wget --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war"
     
    // httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war" 
