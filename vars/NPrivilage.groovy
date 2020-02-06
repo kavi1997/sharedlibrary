8fda948d18e7f98fd32ca2373282ab4179b7ebd8
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-06 19:39:59 +0530

    Create NPrivilage.groovy

---
+++ vars/NPrivilage.groovy
@@ -0,0 +1,16 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.brm)
+
+//String a=jsonObj.data.name
+//String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.password.id
+String pid=b.replaceAll("\\[", "").replaceAll("\\]","");
+sh "curl -X GET -i -H  -d  -u admin:admin123 http://3.15.18.214:8081/nexus/service/local/privileges/${pid}"
+//httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    //httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${pid}/status"
+    
+}
