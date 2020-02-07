e1c572a2714234388e7281e9c2c1d8eb2c1030c1
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-07 06:02:32 +0530

    Update bbnewlog.groovy

--- vars/bbnewlog.groovy
+++ vars/bbnewlog.groovy
@@ -13,18 +13,19 @@ String deploymentprojectid=b.replaceAll("\\[", "").replaceAll("\\]","");
 String deletedprojectname=c.replaceAll("\\[", "").replaceAll("\\]","");
   
  println(message)
-  def mes=message
+  def mes="deployment"
   Date date = new Date() 
-  if(message.contains(deployment))
-     {
+  if ( message.toLowerCase().indexOf(mes.toLowerCase()) != -1 ) {
+
+{
       sh " echo '${date}' Bamboo  ${message} '${deploymentprojectid}' >>log.txt"
      }
-  else if(message.contains(deleted))
+/*  else if(message.contains(deleted))
           {
               sh " echo '${date}' Bamboo  ${message} '${deletedname}' >>log.txt"
           }
      else
      {
        sh " echo '${date}' Bamboo  ${message} '${plankey}' >>log.txt"
-     }
+     }*/
 }
