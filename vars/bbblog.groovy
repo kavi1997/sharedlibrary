93c134a5724dd3f6b9957d724ad99886f52a8bfa
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-07 11:47:56 +0530

    Update and rename bbnewlog.groovy to bbblog.groovy

---
+++ vars/bbblog.groovy
@@ -0,0 +1,19 @@
+def call(message,JSON)
+{
+  
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+println(jsonObj.ci)
+
+
+String a=jsonObj.ci.id.deploymentProjectId
+String deploymentprojectid=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+ println(message)
+  def mes="deployment"
+  Date date = new Date() 
+ 
+      sh " echo '${date}' Bamboo  ${message} '${deploymentprojectid}' >>log.txt"
+    
+     
+}
