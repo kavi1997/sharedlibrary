1017149976725987dbe757237cd5bc6733028caa
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 12:45:56 +0530

    Create Npart_repo.groovy

---
+++ vars/Nexus/Npart_repo.groovy
@@ -0,0 +1,17 @@
+def call(JSON){
+def jsonString =JSON
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.brm)
+
+//String a=jsonObj.data.name
+//String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.repositories.repository.id
+String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+sh "curl -X GET -i -H  -d  -u admin:admin123 http://3.15.18.214:8081/nexus/service/local/repositories/${repoid} "
+//httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    
+   // httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}"
+    
+}
