3a7b5774d91cb652fb78f2e45897dca48dad758e
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-11 18:33:11 +0530

    Update lastbuildtime.groovy

--- vars/lastbuildtime.groovy
+++ vars/lastbuildtime.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh 'curl -s http://18.188.152.185:8080/bamboo1/lastBuild/Time/api/json?tree=estimatedDuration --user vj:11e428e94b267ffbab27fa713e2da8e6e8'
+sh 'curl -s http://18.188.152.185:8080/job/bamboo1/lastBuild/api/json?tree=estimatedDuration --user vj:11e428e94b267ffbab27fa713e2da8e6e8'
 }
