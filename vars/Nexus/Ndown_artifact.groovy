740b33936e73ccf94f14ad748823b29f4e9ba0d3
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 12:44:36 +0530

    Create Ndown_artifact.groovy

---
+++ vars/Nexus/Ndown_artifact.groovy
@@ -0,0 +1,20 @@
+def call(JSON){
+def jsonString = JSON
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.brm)
+
+//String a=jsonObj.brm.password.fileName
+//String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.password.fileName
+String fn=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+
+//httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+   
+  sh "wget --user=admin --password=admin123 'http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/${fn}' "
+    // httpMode: 'GET', wget --user=admin --password=admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war"
+    
+   // httpMode: 'GET', url: "http://3.15.18.214:8081/nexus/service/local/repositories/repo/content/dynamic_new-1.0.war" 
+    
+}
