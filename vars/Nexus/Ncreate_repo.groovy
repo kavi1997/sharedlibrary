88b2e2407cecab93c27bf3e8df67c6e9559d61ce
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 12:38:30 +0530

    Create Ncreate_repo.groovy

---
+++ vars/Nexus/Ncreate_repo.groovy
@@ -0,0 +1,33 @@
+def call(JSON){
+def jsonString = JSON
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println("CREATE REPOSITORY")
+
+String a=jsonObj.brm.repositories.repository.name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.brm.repositories.repository.id
+String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
+	//println(repoName)
+	//println(repoid)
+	
+
+httpRequest authentication: 'nexus_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST', requestBody: 
+  """{
+    	"data":
+	{
+		
+        "id": ${repoid},
+        "name": ${repoName},
+        "repoType": "hosted",
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"       
+	}
+        
+   }""", url: "http://3.15.18.214:8081/nexus/service/local/repositories"
+}
